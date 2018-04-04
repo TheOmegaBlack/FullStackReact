4a7f265e1f2cf7ccedde0e7c9880134037d1bb86
Sacha Morgese <sacha_morgese@waku-2.com>
2018-04-04 18:37:06 +0900

    chore: finished wiring-up google oauth

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '510521705269-7ul1mh5fts07tdjgo3r8q1a9h4eucg90.apps.googleusercontent.com',
+  googleClientSecret: '0w-4EZuQ_JILgycAyuSepEj-',
+}
--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express()
+
+passport.use(
+  new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+    // accesstoken: allows us to reach back to google and confirm we are authorized
+    console.log('access token:', accessToken)
+    // refreshtoken allows us to refresh the token and be re-authorized
+    console.log('refresh token:', refreshToken)
+    // all the info we need
+    console.log('profile:', profile)
+    }
+  )
+)
 
 app.get('/', (req, res) => {
   res.send({hi: 'there'})
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
