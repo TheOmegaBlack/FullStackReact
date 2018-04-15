b5688740b53fed72566a3928ac90a4bd2b06427c
Sacha Morgese <sachamorgese@gmail.com>
2018-04-15 18:05:24 +0900

    add prod.js and keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-module.exports = {
-  googleClientID: '510521705269-7ul1mh5fts07tdjgo3r8q1a9h4eucg90.apps.googleusercontent.com',
-  googleClientSecret: '0w-4EZuQ_JILgycAyuSepEj-',
-  mongoURI: 'mongodb://sacherduck:dasduckunddiehund@ds014118.mlab.com:14118/react_full_stack',
-  cookieKey: 'njwADSfeh3ry898904829rSADAdknd92ie204-^20ADAFF39i489wujeufisd',
-};
+//heroku has this variables already set up
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod')
+} else {
+  module.exports = require('./dev')
+}
