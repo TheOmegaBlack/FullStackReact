6648c97a675861082d0d7bc6315a9de280145705
Sacha Morgese <sachamorgese@gmail.com>
2019-04-13 13:11:02 +0900

    Updated index.js for production Updated dependencies

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("./services/passport");
 
 const uri = keys.mongoURI;
 
-mongoose.connect(uri);
+mongoose.connect(uri, { useNewUrlParser: true });
 
 const app = express();
 
@@ -29,6 +29,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up productions assets
+  // like main.js and main.css
+  app.use(express.static('client/build'));
+  
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
